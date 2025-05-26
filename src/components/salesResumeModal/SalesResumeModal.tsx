576fbb5fd06ff2cd98d2c78bf440ef25eebb1fa0
EricWithC04 <negativegame117@gmail.com>
2025-05-25 22:35:34 -0300

    feat: modal of resume styles

--- src/components/salesResumeModal/SalesResumeModal.tsx
+++ src/components/salesResumeModal/SalesResumeModal.tsx
@@ -1,5 +1,6 @@
 import { forwardRef, useState } from "react"
 import styles from "./SalesResumeModal.module.css"
+import { CreditCard } from "lucide-react"
 
 interface Props {
     closeModal: () => void
@@ -10,9 +11,9 @@ export const SalesResumeModal = forwardRef<HTMLDialogElement, Props> (({ closeMo
     const [filter, setFilter] = useState<number>(4)
 
     const filters = [
-        "Ultimos 7 dias",
-        "Ultimos 30 dias",
-        "Ultimos 90 dias",
+        "Últimos 7 dias",
+        "Últimos 30 dias",
+        "Últimos 90 dias",
         "Limpiar Filtros",
     ]
 
@@ -20,27 +21,57 @@ export const SalesResumeModal = forwardRef<HTMLDialogElement, Props> (({ closeMo
         <dialog ref={ref} className={styles["sales-resume-modal-container"]}>
             <div className={styles["data-container"]}>
                 <h2>Resumen de Ingresos</h2>
-                <div className={styles["filter"]}>
-                    {
-                        filters.map((f, index) => (
-                            <button 
-                                key={index}
-                                onClick={() => setFilter(index+1)}
-                                className={`${filter === index+1 ? styles["selected-filter"] : ""}`}
-                            >{f}</button>
-                        ))
-                    }
+                <div className={styles["filter-container"]}>
+                    <h2>Filtros</h2>
+                    <div className={styles["filter"]}>
+                        {
+                            filters.map((f, index) => (
+                                <button 
+                                    key={index}
+                                    onClick={() => setFilter(index+1)}
+                                    className={`${filter === index+1 && index !== filters.length-1 ? styles["selected-filter"] : ""}`}
+                                >{f}</button>
+                            ))
+                        }
+                    </div>
                 </div>
                 <div className={styles["totals"]}>
                     <div className={styles["one-total"]}>
                         <h4>Ingresos Totales</h4>
+                        <h2>$ 12700</h2>
                     </div>
                     <div className={styles["one-total"]}>
                         <h4>Total de ventas</h4>
+                        <h2>4</h2>
                     </div>
                 </div>
                 <div className={styles["total-per-method"]}>
-                    <h2>Ingresos por Metodo de pago</h2>
+                    <div className={styles["title"]}>
+                        <CreditCard />
+                        <h2>Ingresos por Metodo de pago</h2>
+                    </div>
+                    <div className={styles["methods-container"]}>
+                        <div className={styles["method"]}>
+                            <div className={styles["method-row-up"]}>
+                                <span>Transferencia</span>
+                                <span className={styles["money"]}>$8000</span>
+                            </div>
+                            <div className={styles["method-row-down"]}>
+                                <span>2 ventas</span>
+                                <span>62.99%</span>
+                            </div>
+                        </div>
+                        <div className={styles["method"]}>
+                            <div className={styles["method-row-up"]}>
+                                <span>Efectivo</span>
+                                <span className={styles["money"]}>$4700</span>
+                            </div>
+                            <div className={styles["method-row-down"]}>
+                                <span>2 Ventas</span>
+                                <span>37.01%</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className={styles["buttons-container"]}>
                     <button onClick={closeModal}>Cerrar</button>
