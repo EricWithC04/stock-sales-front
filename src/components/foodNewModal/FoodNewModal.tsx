1f1b2c3804565863feabf57b74feb560463cf0af
EricWithC04 <negativegame117@gmail.com>
2025-05-28 11:45:44 -0300

    feat: new food form data validation

--- src/components/foodNewModal/FoodNewModal.tsx
+++ src/components/foodNewModal/FoodNewModal.tsx
@@ -11,10 +11,21 @@ interface FoodData {
     price: number
 }
 
+interface FoodErrors {
+    name: string
+    category: string
+    description: string
+    price: string
+}
+
 interface Ingredient {
     uid: number
     id: string
     quantity: number
+}
+
+interface IngredientErrors {
+    error: ""
 }
 
 interface Props {
@@ -35,6 +46,15 @@ export const FoodNewModal = forwardRef<HTMLDialogElement, Props>(({ closeModal, 
     const [ingredients, setIngredients] = useState<Array<Ingredient>>([{ uid: 1, id: "", quantity: 0 }])
     const [ingredientFlag, setIngredientFlag] = useState<boolean>(false)
 
+    const [foodErrors, setFoodErrors] = useState<FoodErrors>({
+        name: "",
+        category: "",
+        description: "",
+        price: ""
+    })
+    const [ingredientsErrors, setIngredientsErrors] = useState<Array<IngredientErrors>>([{ error: "" }])
+    const [errorsActive, setErrorsActive] = useState<boolean>(false)
+
     const changeIngredientFlag = () => {
         setIngredientFlag(!ingredientFlag)
     }
@@ -50,11 +70,30 @@ export const FoodNewModal = forwardRef<HTMLDialogElement, Props>(({ closeModal, 
         }
     }
 
+    const validateErrorsData = (data: FoodData): FoodErrors => {
+        const currentErrors: FoodErrors = { name: "", category: "", description: "", price: "" }
+        
+        if (!data.name.length) currentErrors.name = "El nombre es obligatorio"
+        if (!data.category.length) currentErrors.category = "Debes seleccionar la categoría"
+        if (!data.description.length) currentErrors.description = "La descripcion es obligatoria"
+        if (!data.price) currentErrors.price = "El precio es obligatorio"
+        else if (data.price <= 0) currentErrors.price = "El precio debe ser mayor a 0"
+        
+        setFoodErrors(currentErrors)
+        return currentErrors
+    }
+
+    const validateErrorsIngredients = (data: Array<Ingredient>) => {
+
+    }
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
-        setData({
+        const newData = {
             ...data,
             [e.target.name]: e.target.value
-        })
+        }
+        validateErrorsData(newData)
+        setData(newData)
     }
 
     const handleChangeIngredient = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
@@ -67,26 +106,32 @@ export const FoodNewModal = forwardRef<HTMLDialogElement, Props>(({ closeModal, 
     }
 
     const handleSubmit = () => {
-        const newIngredients = ingredients.map(i => ({ id: i.id, quantity: i.quantity }))
-        const newFood = {
-            id: (getFoods().length+1).toString(),
-            name: data.name,
-            description: data.description,
-            price: data.price,
-            ingredients: newIngredients,
-            category: data.category
+        const currentErrors = validateErrorsData(data)
+
+        setErrorsActive(true)
+        if (Object.values(currentErrors).every(error => !error.length)) {
+            const newIngredients = ingredients.map(i => ({ id: i.id, quantity: i.quantity }))
+            const newFood = {
+                id: (getFoods().length+1).toString(),
+                name: data.name,
+                description: data.description,
+                price: data.price,
+                ingredients: newIngredients,
+                category: data.category
+            }
+            setData({
+                name: "",
+                category: "",
+                description: "",
+                price: 0
+            })
+            setIngredients([{ uid: 1, id: "", quantity: 0 }])
+            uploadNewFood(newFood)
+            changeFoods()
+            changeIngredientFlag()
+            setErrorsActive(false)
+            closeModal()
         }
-        setData({
-            name: "",
-            category: "",
-            description: "",
-            price: 0
-        })
-        setIngredients([{ uid: 1, id: "", quantity: 0 }])
-        uploadNewFood(newFood)
-        changeFoods()
-        changeIngredientFlag()
-        closeModal()
     }
 
     const handleCloseModal = () => {
@@ -110,6 +155,10 @@ export const FoodNewModal = forwardRef<HTMLDialogElement, Props>(({ closeModal, 
                         value={data.name}
                     />
                 </div>
+                { 
+                    errorsActive && foodErrors.name.length ? 
+                        <span className={styles["error-message"]}>{foodErrors.name}</span> : null 
+                }
                 <div className={styles["field-container"]}>
                     <label>Categoría</label>
                     <select name="category" onChange={handleChange} defaultValue={""}>
@@ -121,6 +170,10 @@ export const FoodNewModal = forwardRef<HTMLDialogElement, Props>(({ closeModal, 
                         }
                     </select>
                 </div>
+                { 
+                    errorsActive && foodErrors.category.length ? 
+                        <span className={styles["error-message"]}>{foodErrors.category}</span> : null 
+                }
                 <div className={styles["field-container"]}>
                     <div className={styles["ingredient-container-header"]}>
                         <label>Ingredientes</label>
@@ -164,10 +217,18 @@ export const FoodNewModal = forwardRef<HTMLDialogElement, Props>(({ closeModal, 
                         value={data.description}
                     />
                 </div>
+                { 
+                    errorsActive && foodErrors.description.length ? 
+                        <span className={styles["error-message"]}>{foodErrors.description}</span> : null 
+                }
                 <div className={styles["field-container"]}>
                     <label>Precio</label>
                     <input type="number" value={data.price} name="price" placeholder="Ej: 5000" onChange={handleChange}/>
                 </div>
+                { 
+                    errorsActive && foodErrors.price.length ? 
+                        <span className={styles["error-message"]}>{foodErrors.price}</span> : null 
+                }
                 <div className={styles["buttons-container"]}>
                     <button type="button" onClick={handleCloseModal} className={styles["cancel-button"]}>Cancelar</button>
                     <button type="button" onClick={handleSubmit} className={styles["submit-button"]}>Guardar</button>
