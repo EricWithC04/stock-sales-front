4947b658037297836cd3098180bdd28d22ea000d
EricWithC04 <negativegame117@gmail.com>
2025-05-29 12:03:02 -0300

    feat: stadistics page commented for deployment

--- src/pages/stadistics/Stadistics.tsx
+++ src/pages/stadistics/Stadistics.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styles from "./Stadistics.module.css";
+// import React from "react";
+// import styles from "./Stadistics.module.css";
 
 export const StadisticsPage = () => {
     return (
